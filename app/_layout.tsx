feeb2134d6e37c03964e9c73fbf81f4e96819da7
isadia-brian <developerisadia@gmail.com>
2025-05-28 04:46:52 +0300

    nativewind support confirmed and working

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import "@/global.css";
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    Poppins: require("../assets/fonts/Poppins/Poppins-Regular.ttf"),
+    PoppinsBold: require("../assets/fonts/Poppins/Poppins-Bold.ttf"),
+    PoppinsMedium: require("../assets/fonts/Poppins/Poppins-Medium.ttf"),
+    PoppinsSemiBold: require("../assets/fonts/Poppins/Poppins-SemiBold.ttf"),
+    PoppinsLight: require("../assets/fonts/Poppins/Poppins-Light.ttf"),
+  });
+
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
   return <Stack />;
 }
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,ts,tsx}"],
+
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: ["Poppins", "sans-serif"],
+        PoppinsBold: ["PoppinsBold", "sans-serif"],
+        PoppinsMedium: ["PoppinsMedium", "sans-serif"],
+        PoppinsSemiBold: ["PoppinsSemiBold", "sans-serif"],
+        PoppinsLight: ["PoppinsLight", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
